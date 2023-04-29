9e646b0806403d5107bd0389fe85cb630cdc6171
Jonathan Behrens <fintelia@gmail.com>
2023-04-29 15:19:29 -0700

    Better documentation and compression level control

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,18 @@ pub use ktx2::Format;
 
 use crate::dfd_table::TYPE_SIZES;
 
+/// Encode a KTX2 file from a list of image slices. The resulting image will be supercompressed
+/// using Zstandard compression.
+///
+/// # Arguments
+/// * `image_slices` - A list of image slices, each slice is a list of bytes.
+/// * `width` - The width of the image.
+/// * `height` - The height of the image.
+/// * `depth` - The depth of the image (0 if the image is not 3D).
+/// * `layers` - The number of layers (0 if the image is not an array).
+/// * `cubemap` - Whether the image is a cubemap.
+/// * `format` - The format of the image. Used to fill metadata, but not for any sort of conversion.
+/// * `compression_level` - What zstd compression level to use.
 pub fn encode_ktx2(
     image_slices: &[Vec<u8>],
     width: u32,
@@ -13,6 +25,7 @@ pub fn encode_ktx2(
     layers: u32,
     cubemap: bool,
     format: ktx2::Format,
+    compression_level: i32,
 ) -> Vec<u8> {
     if format.0.get() as usize >= DFD_TABLE.len()
         || DFD_TABLE[format.0.get() as usize].is_empty()
@@ -55,7 +68,8 @@ pub fn encode_ktx2(
     let mut compressed_image_slices = Vec::new();
     let mut offset = (80 + 24 * levels + dfd.len() as u32) as u64;
     for image_slice in image_slices {
-        let compressed = zstd::encode_all(std::io::Cursor::new(image_slice), 12).unwrap();
+        let compressed =
+            zstd::encode_all(std::io::Cursor::new(image_slice), compression_level).unwrap();
         contents.extend_from_slice(&offset.to_le_bytes());
         contents.extend_from_slice(&(compressed.len() as u64).to_le_bytes());
         contents.extend_from_slice(&(image_slice.len() as u64).to_le_bytes());
